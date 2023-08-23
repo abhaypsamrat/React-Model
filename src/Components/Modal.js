9e26a02f86a6831e2afbb9ebb38f1a5d4cf4887b
abhaypsamrat <abhay.p.samrat@gmail.com>
2023-08-23 09:31:39 +0530

    change data

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./Modal.css";
-// import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
-// import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
 
 function Modal({ setOpenModal }) {
   return (
@@ -16,50 +14,15 @@ function Modal({ setOpenModal }) {
             X
           </button>
         </div>
-        {/* <div className="body">
-          <Table>
-            <Thead>
-              <Tr>
-                <Th>Screen/Service</Th>
-                <Th>Name of The Screen/Service</Th>
-                <Th>User</Th>
-                <Th>Created Time</Th>
-                <Th>created By</Th>
-                <Th>Age</Th>
-                <Th>Mobile</Th>
-                <Th>Email</Th>
-                <Th>Data</Th>
-                <Th>Operation</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Service</Td>
-                <Td>RG97-RG97-W00001</Td>
-                <Td>prashantp</Td>
-                <Td>18 July 2023</Td>
-                <Td>prashantp</Td>
-                <Td>21</Td>
-                <Td>0000000000</Td>
-                <Td>prashantp@gmail.com</Td>
-                <Td>0001234</Td>
-                <Td>xyzzz</Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </div> */}
-
-
         <div className="table-container">
           <table>
             <thead>
               <tr>
                 <th>Screen/Services</th>
-                <th>NAme of the creen/Services</th>
+                <th>Name of the Screen/Services</th>
                 <th>User</th>
                 <th>Created Time</th>
-                <th>created By</th>
-                <th>Age</th>
+                <th>Created By</th>
                 <th>Email</th>
                 <th>Data</th>
                 <th>Operation</th>
@@ -79,7 +42,6 @@ function Modal({ setOpenModal }) {
                 <td>prashantp</td>
                 <td>20 July 2023</td>
                 <td>prashantp</td>
-                <td>9009090900</td>
                 <td>prashantp@gmail.com</td>
                 <td>xyz</td>
                 <td>Nothing</td>
@@ -90,12 +52,10 @@ function Modal({ setOpenModal }) {
                 <td>helloxyz</td>
                 <td>20 July 2023</td>
                 <td>prashantp</td>
-                <td>9009090900</td>
                 <td>hello@gmail.com</td>
                 <td>xyz</td>
                 <td>Nothing</td>
               </tr>
-             
             </tbody>
           </table>
         </div>
